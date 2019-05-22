ea3c035cbe41c9b7436d264fc9a814289ac3640a
Jaydarian <jayrey.go@gmail.com>
2019-05-21 20:02:45 -0500

    change layout and main route to have the current user.  import current user and url_for into main route

--- lightning_flask/main/routes.py
+++ lightning_flask/main/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, request, Blueprint, url_for
+from flask_login import current_user
 
 from lightning_flask.models import Post
 
@@ -11,7 +12,9 @@ main = Blueprint('main', __name__)
 def home():  # AKA view function
     page = request.args.get('page', 1, type=int)  # by default, add a query parameter to this route. It's default value is 1
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=7)
-    return render_template('home.html', posts=posts)
+    if current_user:
+        image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('home.html', posts=posts, image_file=image_file)
 
 @main.route("/about")
 def about():
